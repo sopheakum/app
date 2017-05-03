1d2a0bb8326e62c0e4182286f2405acc6a6a1a26
Sopheak <sopheak@users-macbook-pro.local>
2017-05-03 19:25:06 +0700

    Initial Commit

---
+++ BUTSTORE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BUTSTORE
+//
+//  Created by Sopheak on 4/30/17.
+//  Copyright © 2017 Sopheak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
